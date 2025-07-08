5f2488f507ba94aa50a9af621405aba89fd5ef2f
abensoft-joaoluis <joao.santos@abensoft.com.br>
2025-07-08 08:59:03 -0300

    factorial

--- other.erl
+++ other.erl
@@ -1,8 +1,15 @@
 -module(other).
 -export([divide/2]).
-
+-export([factorial/1]).
 
 divide(A,B) when B =/= 0 ->
     A div B;
 divide(_,0) ->
     {error,division_by_zero}.
+
+
+
+factorial(1) ->
+    1;
+factorial(N) ->
+    N * factorial(N-1). 
